5c622189b8a03e19a3f2060feaa1512f3180576c
karan10-tech <karanmaurya2910.com>
2025-11-29 13:48:25 +0530

    2

--- app.js
+++ app.js
@@ -4,7 +4,11 @@ import cookieParser from "cookie-parser"
 
 const app = express()
 
-app.use(cors());
+app.use(cors({
+  origin: process.env.CLIENT_URL?.split(',') || ["http://localhost:5173"],
+  credentials: true,
+})
+);
 
 app.use(express.json({ limit: "900kb" }));
 app.use(express.urlencoded({ extended: true, limit: "900kb" }));
